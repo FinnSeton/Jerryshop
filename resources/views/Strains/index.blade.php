1877e783b69b376e0dce2fd4249ba8c62fb295d9
FinnSeton <113095872+FinnSeton@users.noreply.github.com>
2024-11-21 15:00:28 +0100

    .

--- resources/views/Strains/index.blade.php
+++ resources/views/Strains/index.blade.php
@@ -9,22 +9,23 @@
 @endsection
 
 @section('content')
-    <div class="container border-2 rounded border-white mx-auto sm bg-gray-600 text-white" >
+    <div class="container border-2 rounded border-white mx-auto sm bg-gray-800 text-white" >
         <h1 class="ml-5 text-2xl">Hier staan alle strains</h1>
 
+        <div class="bg-gray-600">
+            @foreach($strains as $strain)
+                <div class=pl-5 strain">
 
-        @foreach($strains as $strain)
-            <div class="strain">
+                    {{$strain->naam}} - {{$strain->merk}} - {{$strain->soort}} - {{$strain->thc}}% THC - {{$strain->cbd}}% CBD  - €{{$strain->prijs}}
 
-                {{$strain->naam}} - {{$strain->merk}} -{{$strain->soort}} - {{$strain->thc}}% THC - {{$strain->cbd}}% CBD  - €{{$strain->prijs}}
+                    @foreach($strain->joints as $joint)
+                        {{$joint->id}}
+                        -
+                    @endforeach
 
-                @foreach($strain->joints as $joint)
-                    {{$joint->id}}
-                    -
-                @endforeach
-
-            </div>
-        @endforeach
+                </div>
+            @endforeach
+        </div>    
     </div>
 
 @endsection
